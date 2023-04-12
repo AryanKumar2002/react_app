008e6fbf171579b1f9ecb041b6ce69ffe4cee67b
AryanKumar2002 <akumar54@calpoly.edu>
2023-04-11 19:50:18 -0700

    t

--- src/Table.js
+++ src/Table.js
@@ -29,13 +29,16 @@ function TableBody(props) {
 }
   
 
-  function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
-} 
+function Table (props) {
+  return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+  );
+}
 
 export default Table;
